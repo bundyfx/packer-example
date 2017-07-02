14f075614103b2259f7f22a6d40699c8cdb7089a
Flynn Bundy <f.bundy@coolblue.nl>
2017-07-02 21:48:23 +0200

    change scripts

--- provisioners/powershell/datadog.ps1
+++ provisioners/powershell/datadog.ps1
@@ -1,23 +1,70 @@
+<#
+.Synopsis
+   Allows running processes and capturing output.
+.DESCRIPTION
+   Allows running processes and capturing output.
+.EXAMPLE
+   Start-ProcessWithCapture -FilePath 'git' -ArgumentList 'pull' -WorkingDirectory $Path -ErrorAction Stop
+#>
+function Start-ProcessWithCapture
+{
+    [CmdletBinding()]
+    Param
+    (
+        # File Path of the Process to Execute
+        [Parameter(Mandatory=$true)]
+        [string]
+        $FilePath,
+
+        # Argument List for Process
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ArgumentList,
+
+        # Argument List for Process
+        [Parameter(Mandatory=$false)]
+        [string]
+        $WorkingDirectory
+    )
+
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+
+    if ($PSBoundParameters.ContainsKey('WorkingDirectory'))
+    {
+        $pinfo.WorkingDirectory = $WorkingDirectory
+    }
+
+    $pinfo.FileName = $FilePath
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $ArgumentList
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $p.WaitForExit()
+    $stdout = $p.StandardOutput.ReadToEnd()
+    $stderr = $p.StandardError.ReadToEnd()
+
+    $output = @{}
+    $output.stdout = $stdout
+    $output.stderr = $stderr
+    $output.exitcode = $p.ExitCode
+
+    return $output
+}
+
 New-Item -Path "C:\Temp" -ItemType Directory -Force
 
 Write-Output "Downloading DataDog Agent"
 Invoke-WebRequest -UseBasicParsing -Uri "https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli.msi" -OutFile "C:\Temp\ddagent-cli.msi"
 
 Write-Output "Install the DataDog Agent"
-$file = gci "C:\Temp\ddagent-cli.msi"
 
-$DataStamp = get-date -Format yyyyMMddTHHmmss
-$logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
-$MSIArguments = @(
-    "/i"
-    ('"{0}"' -f $file.fullname)
-    "/qn"
-    "/norestart"
-    "/L*v"
-    $logFile
-)
-Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
-Start-Sleep 15
+$out = Start-ProcessWithCapture -FilePath msiexec -ArgumentList "/qn /i C:\Temp\ddagent-cli.msi"
+
+Write-Output "Wait for DataDog serivce to start"
+Start-Sleep -Seconds 15
 
 Write-Output "Stop the DataDog Service"
 Stop-Service -Name DatadogAgent -Force
@@ -28,3 +75,6 @@ Set-Service -Name DatadogAgent -StartupType Disabled
 Write-Output "Clean up temporary folder"
 Remove-Item -Path "C:\Temp" -Recurse -Force
 
+Write-Output "DataDog Install stdout: $($out.stdout)"
+Write-Output "DataDog Install stderr: $($out.stderr)"
+Write-Output "DataDog Install exitcode: $($out.exitcode)"
--- provisioners/powershell/dotnet_core.ps1
+++ provisioners/powershell/dotnet_core.ps1
@@ -1,12 +1,72 @@
+
+<#
+.Synopsis
+   Allows running processes and capturing output.
+.DESCRIPTION
+   Allows running processes and capturing output.
+.EXAMPLE
+   Start-ProcessWithCapture -FilePath 'git' -ArgumentList 'pull' -WorkingDirectory $Path -ErrorAction Stop
+#>
+function Start-ProcessWithCapture
+{
+    [CmdletBinding()]
+    Param
+    (
+        # File Path of the Process to Execute
+        [Parameter(Mandatory=$true)]
+        [string]
+        $FilePath,
+
+        # Argument List for Process
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ArgumentList,
+
+        # Argument List for Process
+        [Parameter(Mandatory=$false)]
+        [string]
+        $WorkingDirectory
+    )
+
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+
+    if ($PSBoundParameters.ContainsKey('WorkingDirectory'))
+    {
+        $pinfo.WorkingDirectory = $WorkingDirectory
+    }
+
+    $pinfo.FileName = $FilePath
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = $ArgumentList
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $p.WaitForExit()
+    $stdout = $p.StandardOutput.ReadToEnd()
+    $stderr = $p.StandardError.ReadToEnd()
+
+    $output = @{}
+    $output.stdout = $stdout
+    $output.stderr = $stderr
+    $output.exitcode = $p.ExitCode
+
+    return $output
+}
+
 New-Item -Path "C:\Temp" -ItemType Directory -Force
 
 Write-Output "Downloading dotnetcore"
 Invoke-WebRequest -UseBasicParsing -Uri "https://go.microsoft.com/fwlink/?linkid=837808" -OutFile "C:\Temp\dotnetcore.exe"
 
-Write-Output "Installing dotnetcore"
-$CurrentLocation = "c:\temp"
-$exe = "dotnetcore.exe"
-Start-Process -FilePath $CurrentLocation\$exe -ArgumentList "/install /quiet" -wait
+Write-Output "Install dotnetcore"
+
+$out = Start-ProcessWithCapture -FilePath C:\Temp\dotnetcore.exe -ArgumentList "/install /quiet"
 
 Write-Output "Clean up temporary folder"
 Remove-Item -Path "C:\Temp" -Recurse -Force
+
+Write-Output "dotnetcore stdout: $($out.stdout)"
+Write-Output "dotnetcore stderr: $($out.stderr)"
+Write-Output "dotnetcore exitcode: $($out.exitcode)"
